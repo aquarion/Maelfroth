45e3499ed4c4bb0ec7b8ae046402c33bd8b48146
Nicholas Avenell <nicholas@aquarionics.com>
2011-05-24 16:43:26 +0100

    New photographers

--- htdocs/photographers.php
+++ htdocs/photographers.php
@@ -4,5 +4,6 @@ $photographers = array(
 	'Arachne'  => array("name" => "Arachne", "url" => "http://www.flickr.com/photos/ara-maye/"),
 	'Xarra' => array("name" => "Dark Photography", "url" => "http://www.darkphotographic.com/maelstrom.php"),
 	'Dan Osbaldeston' => array("name" => 'Dan Osbaldeston', "url" => "http://www.flickr.com/photos/53081736@N00/"),
-	"Julia Al Stenburg" => array("name" => "Julia Al Stenburg", "url" => "http://www.facebook.com/Laserkatt")
+	"Julia Al Stenburg" => array("name" => "Julia Al Stenburg", "url" => "http://www.facebook.com/Laserkatt"),
+	"Photoboy" => array("name" => "Photoboy", "url" => "http://klae.110mb.com/")
 );
