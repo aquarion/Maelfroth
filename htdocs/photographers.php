77fb0e45f05ab62a4c19dc67cf76e4562d7f9fb5
Nicholas Avenell <nicholas@aquarionics.com>
2012-01-12 18:35:52 +0000

    Add Oliver Facey to list of credited photographers

--- htdocs/photographers.php
+++ htdocs/photographers.php
@@ -5,5 +5,6 @@ $photographers = array(
 	'Xarra' => array("name" => "Dark Photography", "url" => "http://www.darkphotographic.com/maelstrom.php"),
 	'Dan Osbaldeston' => array("name" => 'Dan Osbaldeston', "url" => "http://www.flickr.com/photos/53081736@N00/"),
 	"Julia Al Stenburg" => array("name" => "Julia Al Stenburg", "url" => "http://www.facebook.com/Laserkatt"),
-	"Photoboy" => array("name" => "Photoboy", "url" => "http://klae.110mb.com/")
+	"Photoboy" => array("name" => "Photoboy", "url" => "http://klae.110mb.com/"),
+	'Oliver Facey' => array("name" => "Oliver Facey", "url" => "http://oliverfacey.co.uk")
 );
