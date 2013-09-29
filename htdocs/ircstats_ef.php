3fa5777593cc1649396d0eb20f0c81643b615d39
Nicholas Avenell <nicholas@aquarionics.com>
2013-09-29 21:55:45 +0100

    Do stats for Empirefroth

---
+++ htdocs/ircstats_ef.php
@@ -0,0 +1,25 @@
+<?PHP
+
+$title = "Lies, Damned Lies and Statistics";
+include("header.php");
+
+
+?>
+
+<style type="text/css">
+	@import url('gfx/default.css');
+</style>
+
+<div id="content">
+
+<?PHP
+
+readfile ('empirefroth_stats.html');
+
+?>
+
+
+</div>
+
+</body>
+</html>
